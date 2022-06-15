80c379a8e5cad9c14b4af84a9a2cbde16c258285
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-06-16 00:18:21 +0200

    Add global ConfigModule for using env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './api/auth/auth.module';
 import { PrismaModule } from './api/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
