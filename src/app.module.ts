d12743846ff99943ed1ff70f2a4e8cd0fcb57e69
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-06-16 13:08:44 +0200

    Add test guard to user/:id endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './api/auth/auth.module';
 import { PrismaModule } from './api/prisma/prisma.module';
+import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     PrismaModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
