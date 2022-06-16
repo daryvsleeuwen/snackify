8e7ea6cc42a606b70150db96b4442f690d4adf95
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-06-16 17:29:26 +0200

    Move AuthGuard globally to user controller

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -6,12 +6,20 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { UserService } from './user.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
-  @Get('')
+  constructor(private userService: UserService){}
+
+    @Get('')
   async getUser(@Req() req: Request) {
     return req.user;
+  }
+
+  @Get('/all')
+  async getAllUsers() {
+    return this.userService.getAllUsers();
   }
 }
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  getAllUsers() {
+    return this.prisma.user.findMany();
+  }
+}
