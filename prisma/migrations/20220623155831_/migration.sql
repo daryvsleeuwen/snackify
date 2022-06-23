1aa13a1e2317fabecc9432fabd3cac55eae2ab15
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-06-23 17:59:01 +0200

    Update migrations

--- prisma/migrations/20220623155831_/migration.sql
+++ prisma/migrations/20220623155831_/migration.sql
@@ -4,12 +4,16 @@ CREATE TABLE `User` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
     `profileImage` VARCHAR(191) NOT NULL,
-    `totalEatenSnacks` INTEGER NOT NULL,
-    `totalBrownBreadEaten` INTEGER NOT NULL,
-    `totalWhiteBreadEaten` INTEGER NOT NULL,
-    `snackId` INTEGER NOT NULL,
+    `favouriteSnack` INTEGER NULL,
+    `totalEatenSnacks` INTEGER NOT NULL DEFAULT 0,
+    `totalBrownBreadEaten` INTEGER NOT NULL DEFAULT 0,
+    `totalWhiteBreadEaten` INTEGER NOT NULL DEFAULT 0,
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -20,8 +24,8 @@ CREATE TABLE `Snack` (
     `updatedAt` DATETIME(3) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `image` VARCHAR(191) NOT NULL,
-    `orderId` INTEGER NULL,
 
+    UNIQUE INDEX `Snack_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -39,17 +43,31 @@ CREATE TABLE `Order` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `userId` INTEGER NOT NULL,
+    `whiteBuns` INTEGER NOT NULL DEFAULT 0,
+    `brownBuns` INTEGER NOT NULL DEFAULT 0,
     `sessionId` INTEGER NULL,
+    `userId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- AddForeignKey
-ALTER TABLE `User` ADD CONSTRAINT `User_snackId_fkey` FOREIGN KEY (`snackId`) REFERENCES `Snack`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateTable
+CREATE TABLE `_OrderToSnack` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_OrderToSnack_AB_unique`(`A`, `B`),
+    INDEX `_OrderToSnack_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Snack` ADD CONSTRAINT `Snack_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Order` ADD CONSTRAINT `Order_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Order` ADD CONSTRAINT `Order_sessionId_fkey` FOREIGN KEY (`sessionId`) REFERENCES `Session`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_OrderToSnack` ADD CONSTRAINT `_OrderToSnack_A_fkey` FOREIGN KEY (`A`) REFERENCES `Order`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_OrderToSnack` ADD CONSTRAINT `_OrderToSnack_B_fkey` FOREIGN KEY (`B`) REFERENCES `Snack`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
