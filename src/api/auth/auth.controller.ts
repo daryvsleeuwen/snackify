b2d52a57d4882b768e564f49229dc769ccbc10a3
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-06-30 19:19:29 +0200

    Archive signup endpoint. Re-add in later development

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AccessTokenDto, SignInDto, UserDto } from './dto';
+import { AccessTokenDto, SignInDto } from './dto';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('signup')
-  singup(@Body() data: UserDto) {
-    return this.authService.signup(data);
-  }
+  // @Post('signup')
+  // singup(@Body() data: UserDto) {
+  //   return this.authService.signup(data);
+  // }
 
   @Post('signin')
   singin(@Body() data: SignInDto) {
