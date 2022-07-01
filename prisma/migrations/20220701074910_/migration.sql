b6f7fb22d36fb9ecfe60eb84a6a6bd7cfb439166
daryvsleeuwen <d.vansleeuwen@lameco.nl>
2022-07-01 09:49:37 +0200

    Cleanup migrations

--- prisma/migrations/20220701074910_/migration.sql
+++ prisma/migrations/20220701074910_/migration.sql
@@ -52,12 +52,12 @@ CREATE TABLE `Order` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `_OrderToSnack` (
-    `A` INTEGER NOT NULL,
-    `B` INTEGER NOT NULL,
+CREATE TABLE `SnackToOrder` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `snackId` INTEGER NOT NULL,
+    `orderId` INTEGER NULL,
 
-    UNIQUE INDEX `_OrderToSnack_AB_unique`(`A`, `B`),
-    INDEX `_OrderToSnack_B_index`(`B`)
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
@@ -67,7 +67,7 @@ ALTER TABLE `Order` ADD CONSTRAINT `Order_userId_fkey` FOREIGN KEY (`userId`) RE
 ALTER TABLE `Order` ADD CONSTRAINT `Order_sessionId_fkey` FOREIGN KEY (`sessionId`) REFERENCES `Session`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_OrderToSnack` ADD CONSTRAINT `_OrderToSnack_A_fkey` FOREIGN KEY (`A`) REFERENCES `Order`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `SnackToOrder` ADD CONSTRAINT `SnackToOrder_snackId_fkey` FOREIGN KEY (`snackId`) REFERENCES `Snack`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_OrderToSnack` ADD CONSTRAINT `_OrderToSnack_B_fkey` FOREIGN KEY (`B`) REFERENCES `Snack`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `SnackToOrder` ADD CONSTRAINT `SnackToOrder_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
