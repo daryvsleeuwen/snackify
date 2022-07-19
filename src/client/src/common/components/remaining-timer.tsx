63196e2edc4f8075bfe8dfd3d3a4d3e381caec47
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2022-07-19 22:34:58 +0200

    Change timer

--- src/client/src/common/components/remaining-timer.tsx
+++ src/client/src/common/components/remaining-timer.tsx
@@ -10,19 +10,11 @@ const RemainingTimer = (props: RemainingTimerProps) => {
 
   useEffect(() => {
     const counter = setInterval(() => {
-      //Re add in later development
-      // setRemainingTime(remainingTime - 1000);
-
-      //TEMP FIX
-
       const title = document.querySelector('.section-title');
-      title.setAttribute('remaining-time', (remainingTime - 1000).toString());
 
       title.textContent = `De huidige sessie is nog ${millisecondsToTime(
-        1_800_000 - (Date.now() - parseInt(title.getAttribute('remaining-time')) - 1000),
+        1_800_000 - (Date.now() - remainingTime),
       )} lang geldig`;
-
-      title.setAttribute('remaining-time', (remainingTime - 1000).toString());
     }, 1000);
 
     return () => {
