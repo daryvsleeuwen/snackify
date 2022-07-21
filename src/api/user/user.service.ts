61f584d351439d2d5b8250b5b0ab7a5f640d71f1
daryvsleeuwen <d.vansleeuwen@lameco.nl>
2022-07-21 16:09:04 +0200

    Only select certain user fields

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import {Injectable} from '@nestjs/common';
+import {PrismaService} from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {
+    }
 
-  getAllUsers() {
-    return this.prisma.user.findMany();
-  }
+    getAllUsers() {
+        return this.prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                profileImage: true
+            }
+        });
+    }
 }
